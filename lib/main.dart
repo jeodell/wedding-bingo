a630e443ea9b4f67d398d368c42c6e605eda709c
jeodell <jeodell@ncsu.edu>
2022-04-15 23:00:54 -0400

    Initial version with user select and a few entries

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,39 +9,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wedding Bingo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Wedding Bingo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +31,120 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _currentGuest = '';
+  List<DropdownMenuItem<dynamic>> dropdownItems = [];
 
-  void _incrementCounter() {
+  void _onGuestChanged(dynamic value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentGuest = value;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (_currentGuest == '') {
+      for (var name in guests) {
+        dropdownItems.add(DropdownMenuItem(
+          child: Text(name),
+          value: name,
+        ));
+      }
+    }
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(child: Text(widget.title)),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: _currentGuest == ''
+          ? Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Who Are You?',
+                ),
+                DropdownButtonFormField(
+                    items: dropdownItems, onChanged: _onGuestChanged),
+              ],
+            ))
+          : Padding(
+              padding: const EdgeInsets.all(1),
+              child: SizedBox(
+                child: GridView.count(
+                  crossAxisCount: 5,
+                  mainAxisSpacing: 1,
+                  crossAxisSpacing: 1,
+                  children: List.generate(25, (index) {
+                    final currentMap = conditions
+                        .elementAt(Random().nextInt(conditions.length));
+                    final currentVictim = currentMap.keys.first;
+                    final currentCondition = currentMap[currentVictim];
+                    return Stack(
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'images/$currentVictim.jpg',
+                            height: 250,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.topCenter,
+                          color: Colors.black.withOpacity(0.5),
+                          child: Text(
+                            currentCondition!,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  }),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+const guests = [
+  'Brianna',
+  'Jason',
+  'Jared',
+  'Lindsey',
+  'Nathan',
+  'Rachel',
+  'Chris',
+  'Amanda',
+  'Derek',
+  'Steve',
+  'Rhonda',
+  'Janice',
+  'Larry',
+  'Tracy',
+  'Mary',
+];
+
+const conditions = [
+  {'steve': 'Steve says \'Nice.\''},
+  {'nathan': 'Nathan does knees over toes'},
+  {'nathan': 'Nathan or Jared talks about crypto or NFTs'},
+  {'brianna': 'Brianna cries'},
+  {'steve': 'Steve mentions Jason\'s \'Big Brain\''},
+  {'steve': 'Steve ignores someone while on his phone'},
+  {'steve': 'Steve says \'Nice.\''},
+  {'nathan': 'Nathan does knees over toes'},
+  {'nathan': 'Nathan or Jared talks about crypto or NFTs'},
+  {'brianna': 'Brianna cries'},
+  {'steve': 'Steve mentions Jason\'s \'Big Brain\''},
+  {'steve': 'Steve ignores someone while on his phone'},
+  {'steve': 'Steve says \'Nice.\''},
+  {'nathan': 'Nathan does knees over toes'},
+  {'nathan': 'Nathan or Jared talks about crypto or NFTs'},
+  {'brianna': 'Brianna cries'},
+  {'steve': 'Steve mentions Jason\'s \'Big Brain\''},
+  {'steve': 'Steve ignores someone while on his phone'},
+];
