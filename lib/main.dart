da5034763c88d65f488dc0834129517d8dab0496
Jason O'Dell <jason@vitalflohealth.com>
2022-07-02 00:01:33 -0400

    Updating fonts in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:wedding_bingo/routes/router.gr.dart';
 
 void main() {
@@ -14,7 +15,26 @@ class Wedding extends StatelessWidget {
     return MaterialApp.router(
       title: 'Collective Bug',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blueGrey,
+        ).copyWith(
+          secondary: Colors.redAccent,
+        ),
+        textTheme: TextTheme(
+          headline1: GoogleFonts.marcellus(),
+          headline2: GoogleFonts.marcellus(),
+          headline3: GoogleFonts.marcellus(),
+          headline4: GoogleFonts.marcellus(),
+          headline5: GoogleFonts.marcellus(),
+          headline6: GoogleFonts.marcellus(),
+          subtitle1: GoogleFonts.josefinSans(),
+          subtitle2: GoogleFonts.josefinSans(),
+          bodyText1: GoogleFonts.josefinSans(),
+          bodyText2: GoogleFonts.josefinSans(),
+          button: GoogleFonts.josefinSans(),
+          caption: GoogleFonts.josefinSans(),
+          overline: GoogleFonts.josefinSans(),
+        ),
       ),
       routerDelegate: appRouter.delegate(),
       routeInformationParser: appRouter.defaultRouteParser(),
