adb921cfeb51a9026d56b251a64f272d948b17bc
Jason O'Dell <jason@vitalflohealth.com>
2022-07-03 14:08:24 -0400

    Missing commas

--- lib/welcome/welcome.dart
+++ lib/welcome/welcome.dart
@@ -23,49 +23,50 @@ class Welcome extends StatelessWidget {
                   fit: BoxFit.cover),
             ),
             child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: Container(
-                  alignment: Alignment.topLeft,
-                  padding: const EdgeInsets.fromLTRB(36, 36, 0, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const <Widget>[
-                      Text(
-                        'Welcome',
-                        style: TextStyle(
-                          color: Color(0xff46638E),
-                          fontSize: 48,
-                          fontFamily: 'Marcellus',
-                        ),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.fromLTRB(36, 36, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text(
+                      'Welcome',
+                      style: TextStyle(
+                        color: Color(0xff46638E),
+                        fontSize: 48,
+                        fontFamily: 'Marcellus',
                       ),
-                      SizedBox(height: 24),
-                      Text(
-                        "We can't wait to see you at",
-                        style: TextStyle(
-                          color: Color(0xff46638E),
-                          fontSize: 14,
-                        ),
+                    ),
+                    SizedBox(height: 24),
+                    Text(
+                      "We can't wait to see you at",
+                      style: TextStyle(
+                        color: Color(0xff46638E),
+                        fontSize: 14,
                       ),
-                      SizedBox(height: 12),
-                      Text(
-                        'A Bug Wedding',
-                        style: TextStyle(
-                          color: Color(0xff46638E),
-                          fontSize: 28,
-                        ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      'A Bug Wedding',
+                      style: TextStyle(
+                        color: Color(0xff46638E),
+                        fontSize: 28,
                       ),
-                      SizedBox(height: 36),
-                      Text(
-                        'September 11-18, 2022',
-                        style: TextStyle(
-                          color: Color(0xff46638E),
-                          fontSize: 16,
-                        ),
+                    ),
+                    SizedBox(height: 36),
+                    Text(
+                      'September 11-18, 2022',
+                      style: TextStyle(
+                        color: Color(0xff46638E),
+                        fontSize: 16,
                       ),
-                    ],
-                  ),
-                )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
