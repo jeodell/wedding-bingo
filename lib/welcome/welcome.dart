39b645cb63552f99559c1a9473cef37c11b0a2b9
Jason O'Dell <jason@vitalflohealth.com>
2022-07-03 21:56:19 -0400

    Removing unnecessary stack

--- lib/welcome/welcome.dart
+++ lib/welcome/welcome.dart
@@ -12,63 +12,59 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                    'images/welcome.jpg',
-                  ),
-                  fit: BoxFit.cover),
-            ),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.fromLTRB(36, 36, 0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const <Widget>[
-                    Text(
-                      'Welcome',
-                      style: TextStyle(
-                        color: Color(0xff46638E),
-                        fontSize: 48,
-                        fontFamily: 'Marcellus',
-                      ),
-                    ),
-                    SizedBox(height: 24),
-                    Text(
-                      "We can't wait to see you at",
-                      style: TextStyle(
-                        color: Color(0xff46638E),
-                        fontSize: 14,
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    Text(
-                      'A Bug Wedding',
-                      style: TextStyle(
-                        color: Color(0xff46638E),
-                        fontSize: 28,
-                      ),
-                    ),
-                    SizedBox(height: 36),
-                    Text(
-                      'September 11-18, 2022',
-                      style: TextStyle(
-                        color: Color(0xff46638E),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                'assets/images/welcome.jpg',
               ),
+              fit: BoxFit.cover),
+        ),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.fromLTRB(36, 36, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                Text(
+                  'Welcome',
+                  style: TextStyle(
+                    color: Color(0xff46638E),
+                    fontSize: 48,
+                    fontFamily: 'Marcellus',
+                  ),
+                ),
+                SizedBox(height: 24),
+                Text(
+                  "We can't wait to see you at",
+                  style: TextStyle(
+                    color: Color(0xff46638E),
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(height: 12),
+                Text(
+                  'A Bug Wedding',
+                  style: TextStyle(
+                    color: Color(0xff46638E),
+                    fontSize: 28,
+                  ),
+                ),
+                SizedBox(height: 36),
+                Text(
+                  'September 11-18, 2022',
+                  style: TextStyle(
+                    color: Color(0xff46638E),
+                    fontSize: 16,
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
