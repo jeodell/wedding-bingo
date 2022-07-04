59af90b2ab008ab43b8b96c9691106347d3690e6
Jason O'Dell <jason@vitalflohealth.com>
2022-07-03 21:56:29 -0400

    Base layout for schedule page

--- lib/schedule/schedule.dart
+++ lib/schedule/schedule.dart
@@ -10,9 +10,28 @@ class Schedule extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Schedule'),
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        color: const Color(0xffbfc9cb),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                'assets/images/mountain.jpg',
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                'Trip Schedule',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 48,
+                  fontFamily: 'Marcellus',
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
