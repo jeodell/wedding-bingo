56afdba5e4c48d8c4c1fdf43e94638400d465661
jeodell <jeodell@ncsu.edu>
2022-07-11 01:13:55 -0400

    Fixing import and making variable final

--- lib/activities/restaurants.dart
+++ lib/activities/restaurants.dart
@@ -1,13 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:wedding_bingo/activities/activityStack.dart';
+import 'package:wedding_bingo/activities/activity_stack.dart';
 
 class Restaurants extends StatelessWidget {
   const Restaurants({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Widget activityWidget = Center(child: Text(context.router.current.name));
+    final Widget activityWidget =
+        Center(child: Text(context.router.current.name));
 
     return buildActivityStack(context, activityWidget);
   }
