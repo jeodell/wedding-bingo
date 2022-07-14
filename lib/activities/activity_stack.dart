790238528a78b2791e2ce68daa9d2159f9ee61c3
Jason O'Dell <jason@vitalflohealth.com>
2022-07-14 11:52:07 -0400

    Adding swipe gesture detector to activity pages for going back

--- lib/activities/activity_stack.dart
+++ lib/activities/activity_stack.dart
@@ -3,16 +3,36 @@ import 'package:flutter/material.dart';
 
 Widget buildActivityStack(BuildContext context, Widget activityWidget) {
   return Scaffold(
-    body: Stack(
-      children: <Widget>[
-        activityWidget,
-        IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            context.router.pop();
-          },
-        )
-      ],
+    body: GestureDetector(
+      onHorizontalDragUpdate: (DragUpdateDetails details) {
+        const int sensitivity = 16;
+        if (details.delta.dx > sensitivity) {
+          context.router.pop();
+        }
+      },
+      child: Stack(
+        children: <Widget>[
+          activityWidget,
+          IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+              size: 32,
+              shadows: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black87,
+                  blurStyle: BlurStyle.outer,
+                  blurRadius: 8,
+                  offset: Offset(3, 3),
+                ),
+              ],
+            ),
+            onPressed: () {
+              context.router.pop();
+            },
+          )
+        ],
+      ),
     ),
   );
 }
