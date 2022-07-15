b783b1aba9ad92edf2e48c1849d4f19ef787c7d6
Jason O'Dell <jason@vitalflohealth.com>
2022-07-15 00:08:10 -0400

    Adding more trails

--- lib/activities/trails.dart
+++ lib/activities/trails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:wedding_bingo/activities/activity_details.dart';
 import 'package:wedding_bingo/activities/activity_stack.dart';
 import 'package:wedding_bingo/theme/colors.dart';
@@ -34,21 +35,73 @@ class Trails extends StatelessWidget {
                   Spacer(),
                 ],
               ),
-              const SizedBox(height: 12),
-              buildAtivityLabel('Icicle Gorge Nature Loop'),
+              const SizedBox(height: 24),
+              buildActivityLabel('Icicle Gorge Nature Loop', WeddingColors.sage,
+                  WeddingColors.tahoe),
+              buildActivityDetails(<String>[
+                '4.2 miles',
+                '150 ft elevation gain',
+                '35 min drive from house',
+                'Impressive rushing creek',
+                'Popular, easy hike'
+              ], WeddingColors.tahoe),
+              buildActivityReadMore(
+                  'https://www.wta.org/go-hiking/hikes/icicle-gorge-loop',
+                  'https://goo.gl/maps/o8cKuRSGQuWjdt3d6',
+                  WeddingColors.tahoe),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'Rat Creek Ridge Trail at Mountain Home Preserve',
+                  WeddingColors.sage,
+                  WeddingColors.tahoe),
+              buildActivityDetails(<String>[
+                '3.4 miles',
+                '800ft elevation gain',
+                '15 min drive from house',
+                'Steep at the beginning but levels off with amazing views of Leavenworth and the surrounding mountains',
+                'Not well marked so bring map',
+              ], WeddingColors.tahoe),
+              buildActivityReadMore(
+                  'https://www.cdlandtrust.org/trails-access/trails/mountain-home-trails?fbclid=IwAR07dJxnaytix-lWtwS9__jc7VEZJuAMDgjm8jg9k0PzjudZHpw_FooZJ5I',
+                  'https://goo.gl/maps/CS8TADiTfrLFWCMPA',
+                  WeddingColors.tahoe),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'Blackbird Island, Waterfront Park in Downtown Leavenworth',
+                  WeddingColors.sage,
+                  WeddingColors.tahoe),
+              buildActivityDetails(<String>[
+                '2 miles',
+                '100ft elevation gain',
+                '7 min drive from house',
+                'River and mountain views from beautiful park in town',
+              ], WeddingColors.tahoe),
+              buildActivityReadMore(
+                  'https://www.wta.org/go-hiking/hikes/blackbird-island-waterfront-park',
+                  'https://goo.gl/maps/75UCrprMdpztqoxV8',
+                  WeddingColors.tahoe),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'MORE Trail Guides', WeddingColors.sage, WeddingColors.tahoe),
               Padding(
                 padding: const EdgeInsets.only(left: 64),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    buildActivityDetails('4.2 miles'),
-                    buildActivityDetails('150ft elevation gain'),
-                    buildActivityDetails('35 min drive from house'),
-                    buildActivityDetails('Impressive rushing creek'),
-                    buildActivityDetails('Popular, easy hike'),
-                  ],
-                ),
-              )
+                child: InkWell(
+                    child: const Text(
+                      '\u2022 The 7 Best Leavenworth Hikes (Besides the Enchantments)',
+                      style: TextStyle(
+                        color: WeddingColors.tahoe,
+                        fontSize: 18,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    onTap: () {
+                      final Uri trailsUri = Uri.parse(
+                          'https://smalltownwashington.com/leavenworth-hikes/');
+                      launchUrl(trailsUri);
+                      return;
+                    }),
+              ),
+              const SizedBox(height: 48),
             ],
           ),
         ),
