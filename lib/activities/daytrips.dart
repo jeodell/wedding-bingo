f18d2df1e1513bdc9bb5711350945114589e4256
Jason O'Dell <jason@vitalflohealth.com>
2022-07-15 16:40:37 -0400

    Starting on daytrips and other pages

--- lib/activities/daytrips.dart
+++ lib/activities/daytrips.dart
@@ -1,14 +1,61 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:wedding_bingo/activities/activity_details.dart';
 import 'package:wedding_bingo/activities/activity_stack.dart';
+import 'package:wedding_bingo/theme/colors.dart';
+import 'package:wedding_bingo/widgets/widgets.dart';
 
 class DayTrips extends StatelessWidget {
   const DayTrips({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final Widget activityWidget =
-        Center(child: Text(context.router.current.name));
+    final Widget activityWidget = Scaffold(
+      body: Container(
+        height: double.infinity,
+        color: WeddingColors.pine,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/coast.jpg',
+              ),
+              const SizedBox(height: 24),
+              Row(
+                children: const <Widget>[
+                  Spacer(),
+                  Text(
+                    'Day Trips',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 48,
+                      fontFamily: 'Marcellus',
+                    ),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'The Watershed Cafe', WeddingColors.rain, Colors.white70),
+              buildActivityDetails(<String>[
+                'Farm to Table',
+                'Pacific Northwest Cuisine',
+                'Downtown Leavenworth',
+                'Dinner Only, closed Tues & Wed',
+              ], Colors.white70),
+              buildActivityReadMore('https://www.watershedpnw.com/',
+                  'https://g.page/WatershedPNW?share', Colors.white70),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'Bavarian Bakery', WeddingColors.rain, Colors.white70),
+              buildActivityDetails(<String>[], Colors.white70),
+              buildBottomPadding(),
+            ],
+          ),
+        ),
+      ),
+    );
 
     return buildActivityStack(context, activityWidget);
   }
--- lib/activities/other.dart
+++ lib/activities/other.dart
@@ -1,14 +1,81 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:wedding_bingo/activities/activity_details.dart';
 import 'package:wedding_bingo/activities/activity_stack.dart';
+import 'package:wedding_bingo/theme/colors.dart';
+import 'package:wedding_bingo/widgets/widgets.dart';
 
 class Other extends StatelessWidget {
   const Other({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final Widget activityWidget =
-        Center(child: Text(context.router.current.name));
+    final Widget activityWidget = Scaffold(
+      body: Container(
+        height: double.infinity,
+        color: WeddingColors.pine,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/blueridge.jpg',
+              ),
+              const SizedBox(height: 24),
+              Row(
+                children: const <Widget>[
+                  Spacer(),
+                  Text(
+                    'Other',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 48,
+                      fontFamily: 'Marcellus',
+                    ),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'Leavenworth Golf Club', WeddingColors.rain, Colors.white70),
+              buildActivityDetails(<String>[
+                r'9 Holes M-Th - $30.00',
+                r'18 Holes M-Th - $40.00',
+                r'18 Holes F-Su & Holidays - $55.00',
+              ], Colors.white70),
+              buildActivityReadMore('https://leavenworthgolf.com/',
+                  'https://goo.gl/maps/Bphb2v6qhDhxoJiBA', Colors.white70),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'Ohme Gardens', WeddingColors.rain, Colors.white70),
+              buildActivityDetails(<String>[
+                'Open 9-6 every day',
+                r'$8 per person',
+              ], Colors.white70),
+              buildActivityReadMore('https://www.ohmegardens.org/',
+                  'https://goo.gl/maps/6sErN4gYTC8Y4hfM8', Colors.white70),
+              const SizedBox(height: 24),
+              buildActivityLabel(
+                  'Aspen Leaf Spa at Sleeping Lady Mountain Resort',
+                  WeddingColors.rain,
+                  Colors.white70),
+              buildActivityDetails(<String>[], Colors.white70),
+              buildActivityReadMore('https://www.sleepinglady.com/spa/spa-menu',
+                  'https://g.page/aspen-leaf-day-spa?share', Colors.white70),
+              const SizedBox(height: 24),
+              buildActivityLabel('Studio 1890 Old Time Photos',
+                  WeddingColors.rain, Colors.white70),
+              buildActivityDetails(<String>[
+                'Theme choices: Baravarian, Civil War, Roaring 20s, Pioneer, Saloon, Victorian, Wild Wild West, Wedding'
+              ], Colors.white70),
+              buildActivityReadMore('https://www.studio1890.com/',
+                  'https://goo.gl/maps/V6XsVF5uhomHaU1v8', Colors.white70),
+              buildBottomPadding(),
+            ],
+          ),
+        ),
+      ),
+    );
 
     return buildActivityStack(context, activityWidget);
   }
