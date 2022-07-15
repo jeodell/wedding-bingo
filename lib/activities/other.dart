b802520aa59ac34796b807508927349e615f1f24
Jason O'Dell <jason@vitalflohealth.com>
2022-07-15 00:07:56 -0400

    Putting garden, golf, and spa into other

--- lib/activities/other.dart
+++ lib/activities/other.dart
@@ -2,8 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:wedding_bingo/activities/activity_stack.dart';
 
-class Spa extends StatelessWidget {
-  const Spa({super.key});
+class Other extends StatelessWidget {
+  const Other({super.key});
 
   @override
   Widget build(BuildContext context) {
