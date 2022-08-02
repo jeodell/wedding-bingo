00c9bd40ff9b0af4e9cd61e875e6a3940e4b06dc
Jason O'Dell <jason@vitalflohealth.com>
2022-08-01 21:52:30 -0400

    Build text with link for memes

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Widget buildBottomPadding() {
   return const SizedBox(height: 48);
@@ -15,5 +16,30 @@ Widget buildText(String text,
     textAlign: align,
     style: style,
     textScaleFactor: 1.0,
+  );
+}
+
+Widget buildTextWithLink(String text, String link, Color color) {
+  return InkWell(
+    onTap: () async {
+      final Uri linkUri = Uri.parse(link);
+      if (await canLaunchUrl(linkUri)) {
+        launchUrl(linkUri);
+      } else {
+        throw 'Could not launch $linkUri';
+      }
+    },
+    child: Row(
+      children: <Widget>[
+        buildText(
+          text,
+          TextStyle(
+            color: color,
+            fontSize: 18,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      ],
+    ),
   );
 }
