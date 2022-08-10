beda391964ccd1c20552086347efa7778f563e31
Jason O'Dell <jason@vitalflohealth.com>
2022-08-09 20:18:01 -0400

    Adding more quiz questions

--- lib/data/quiz_data.dart
+++ lib/data/quiz_data.dart
@@ -1,16 +1,7 @@
 mixin QuizData {
   static final List<Map<String, dynamic>> quiz = [
     {
-      'question': "Who are the enemies in the 1998 classic A Bug's Life?",
-      'answers': [
-        {'answerText': 'Wasps', 'isCorrect': true},
-        {'answerText': 'Grasshoppers', 'isCorrect': false},
-        {'answerText': 'Fleas', 'isCorrect': false},
-        {'answerText': 'Caterpillars', 'isCorrect': false},
-      ],
-    },
-    {
-      'question': 'What song did Brianna tumble to in her 2001 recital?',
+      'question': 'What song did B tumble to in her 2001 recital?',
       'answers': [
         {'answerText': "A Bug's Life", 'isCorrect': true},
         {'answerText': 'Hard Knock Life', 'isCorrect': false},
@@ -18,120 +9,195 @@ mixin QuizData {
         {'answerText': 'Somewhere Over The Rainbow', 'isCorrect': false},
       ]
     },
-    // TODO re-word
     {
       'question': 'Where did sparks first fly?',
       'answers': [
-        {'answerText': 'Lake Lure', 'isCorrect': true},
         {'answerText': 'NC State', 'isCorrect': false},
         {'answerText': 'Calvary', 'isCorrect': false},
+        {'answerText': 'Lake Lure', 'isCorrect': true},
         {'answerText': 'High School Graduation', 'isCorrect': false},
       ],
     },
-    // {
-    //   'question': "What is Brianna's favorite feature of Jason?",
-    //   'answers': [
-    //     {'answerText': 'Butt', 'isCorrect': true},
-    //     {'answerText': 'Smile', 'isCorrect': false},
-    //     {'answerText': 'Big Brain', 'isCorrect': false},
-    //     {'answerText': 'Cool Hair', 'isCorrect': false},
-    //   ],
-    // },
-    // // or
-    // {
-    //   'question': "What is Brianna's favorite feature of Jason?",
-    //   'answers': [
-    //     {'answerText': 'Long Back', 'isCorrect': true},
-    //     {'answerText': 'Bony Shoulders', 'isCorrect': false},
-    //     {'answerText': 'Big Brain', 'isCorrect': false},
-    //     {'answerText': 'All of the Above', 'isCorrect': false},
-    //   ],
-    // },
-    // {
-    //   'question': 'Where did Jason finally propose to Brianna?',
-    //   'answers': [
-    //     {'answerText': 'Tahoe', 'isCorrect': true},
-    //     {'answerText': 'Wind River Range', 'isCorrect': false},
-    //     {'answerText': 'Maine', 'isCorrect': false},
-    //     {'answerText': 'Sawtooth Mountains', 'isCorrect': false},
-    //   ],
-    // },
-    // {
-    //   'question': 'What was not on the first album Jason burned for Brianna?',
-    //   'answers': [
-    //     {'answerText': 'Sk8r Boi', 'isCorrect': true},
-    //     {'answerText': '', 'isCorrect': false},
-    //     {'answerText': '', 'isCorrect': false},
-    //     {'answerText': '', 'isCorrect': false},
-    //   ],
-    // },
-    // {
-    //   'question': 'Where was our first date?',
-    //   'answers': [
-    //     {'answerText': 'Caribou Coffee', 'isCorrect': true},
-    //     {'answerText': 'Morning Times', 'isCorrect': false},
-    //     {'answerText': 'Port City Java', 'isCorrect': false},
-    //     {'answerText': 'Cup a Joe', 'isCorrect': false},
-    //   ]
-    // },
-    // {
-    //   'question': 'Where did the term bugs come from?',
-    //   'answers': [
-    //     {
-    //       'answerText': 'When J received a random snapchat from HayBug1992',
-    //       'isCorrect': true
-    //     },
-    //     {
-    //       'answerText': 'J put his hair up in two antennae-like sprouts',
-    //       'isCorrect': false
-    //     },
-    //     {'answerText': '', 'isCorrect': false},
-    //     {'answerText': '', 'isCorrect': false},
-    //   ],
-    // },
-    // {
-    //   'question': 'What is the most physical pain Brianna has inflicted on J?',
-    //   'answers': [
-    //     {
-    //       'answerText': 'Hitting him in the balls with a nerf basketball',
-    //       'isCorrect': true
-    //     },
-    //     {'answerText': 'Piercing his ear', 'isCorrect': false},
-    //     {
-    //       'answerText':
-    //           'Shoulder checking his chin while attempting to recreate the Dirty Dancing scene',
-    //       'isCorrect': false
-    //     },
-    //     {'answerText': '', 'isCorrect': false},
-    //   ]
-    // },
-    // {
-    //   'question': 'Where was our first hike?',
-    //   'answers': [
-    //     {'answerText': 'Jordan Lake', 'isCorrect': true},
-    //     {'answerText': 'Umstead Park', 'isCorrect': false},
-    //     {'answerText': 'Hanging Rock', 'isCorrect': false},
-    //     {'answerText': 'Grandfather Mountain', 'isCorrect': false},
-    //   ],
-    // },
-    // {
-    //   'question': 'What was the first national park we visited?',
-    //   'answers': [
-    //     {'answerText': 'Great Sand Dunes, CO', 'isCorrect': true},
-    //     {'answerText': 'Hot Springs, AR', 'isCorrect': false},
-    //     {'answerText': 'Rocky Mountain, CO', 'isCorrect': false},
-    //     {'answerText': 'Great Smoky Mountains, NC', 'isCorrect': false},
-    //   ],
-    // },
-    // {
-    //   'question':
-    //       'How many guys were seriously interested in Brianna when J asked her out?',
-    //   'answers': [
-    //     {'answerText': '1', 'isCorrect': true},
-    //     {'answerText': '2', 'isCorrect': false},
-    //     {'answerText': '3', 'isCorrect': false},
-    //     {'answerText': '4', 'isCorrect': false},
-    //   ],
-    // }
+    {
+      'question': "What is B's favorite feature about J?",
+      'answers': [
+        {'answerText': 'Long Back', 'isCorrect': false},
+        {'answerText': 'Bony Shoulders', 'isCorrect': false},
+        {'answerText': 'Big Brain', 'isCorrect': false},
+        {'answerText': 'All of the Above', 'isCorrect': true},
+      ],
+    },
+    {
+      'question': 'Where did J finally propose to B?',
+      'answers': [
+        {'answerText': 'Tahoe', 'isCorrect': false},
+        {'answerText': 'Wind River Range', 'isCorrect': false},
+        {'answerText': 'Maine', 'isCorrect': false},
+        {'answerText': 'Sawtooth Mountains', 'isCorrect': true},
+      ],
+    },
+    {
+      'question': "Who are the enemies in the 1998 classic A Bug's Life?",
+      'answers': [
+        {'answerText': 'Wasps', 'isCorrect': false},
+        {'answerText': 'Grasshoppers', 'isCorrect': true},
+        {'answerText': 'Fleas', 'isCorrect': false},
+        {'answerText': 'Caterpillars', 'isCorrect': false},
+      ],
+    },
+    {
+      'question': 'What was not on the first album J burned for B?',
+      'answers': [
+        {'answerText': 'Sk8r Boi', 'isCorrect': true},
+        {'answerText': '', 'isCorrect': false},
+        {'answerText': '', 'isCorrect': false},
+        {'answerText': '', 'isCorrect': false},
+      ],
+    },
+    {
+      'question': 'Where was our first date?',
+      'answers': [
+        {'answerText': 'Morning Times', 'isCorrect': false},
+        {'answerText': 'Port City Java', 'isCorrect': false},
+        {'answerText': 'Cup a Joe', 'isCorrect': false},
+        {'answerText': 'Caribou Coffee', 'isCorrect': true},
+      ]
+    },
+    {
+      'question': 'Where did the term bugs come from?',
+      'answers': [
+        {
+          'answerText': 'When J received a random snapchat from HayBug1992',
+          'isCorrect': true
+        },
+        {
+          'answerText': 'J put his hair up in two antennae-like sprouts',
+          'isCorrect': false
+        },
+        {
+          'answerText':
+              "B saw an inchworm on NC State's campus and said it looked like J",
+          'isCorrect': false
+        },
+        {
+          'answerText': 'We googled cute pet names for boyfriend/girlfriend',
+          'isCorrect': false
+        },
+      ],
+    },
+    {
+      'question': 'What is the most physical pain B has inflicted on J?',
+      'answers': [
+        {'answerText': 'Piercing his ear', 'isCorrect': true},
+        {
+          'answerText':
+              'Shoulder checking his chin while attempting to recreate the Dirty Dancing scene',
+          'isCorrect': false
+        },
+        {
+          'answerText': 'Tackling him onto a bed of prong-exposed chargers',
+          'isCorrect': false
+        },
+        {
+          'answerText': 'Hitting him in the balls with a nerf basketball',
+          'isCorrect': false
+        },
+      ]
+    },
+    {
+      'question': 'Where was our first hike?',
+      'answers': [
+        {'answerText': 'Jordan Lake', 'isCorrect': false},
+        {'answerText': 'Umstead Park', 'isCorrect': false},
+        {'answerText': 'Hanging Rock', 'isCorrect': false},
+        {'answerText': 'Grandfather Mountain', 'isCorrect': true},
+      ],
+    },
+    {
+      'question': 'What was the first national park we visited?',
+      'answers': [
+        {'answerText': 'Hot Springs, AR', 'isCorrect': false},
+        {'answerText': 'Great Sand Dunes, CO', 'isCorrect': true},
+        {'answerText': 'Rocky Mountain, CO', 'isCorrect': false},
+        {'answerText': 'Great Smoky Mountains, NC', 'isCorrect': false},
+      ],
+    },
+    {
+      'question':
+          'How many guys were seriously interested in B when J asked her out?',
+      'answers': [
+        {'answerText': '1', 'isCorrect': false},
+        {'answerText': '2', 'isCorrect': false},
+        {'answerText': '3', 'isCorrect': false},
+        {'answerText': '4', 'isCorrect': true},
+      ],
+    },
+    {
+      'question': "What is J's favorite feature about B?",
+      'answers': [
+        {'answerText': 'Short Stature', 'isCorrect': true},
+        {'answerText': 'Poop-Brown Eyes', 'isCorrect': false},
+        {'answerText': 'Large Nostrils', 'isCorrect': false},
+        {'answerText': 'Short Eyelashes', 'isCorrect': false},
+      ],
+    },
+    {
+      'question':
+          'What famous dish did J make during his first Haferman Thanksgiving?',
+      'answers': [
+        {'answerText': 'Rolls', 'isCorrect': false},
+        {'answerText': 'Mac n Cheese', 'isCorrect': true},
+        {'answerText': 'Mashed Potatoes', 'isCorrect': false},
+        {'answerText': 'Green Beans', 'isCorrect': false},
+      ],
+    },
+    {
+      'question':
+          'How many times did J and B sleep in a car during their trips?',
+      'answers': [
+        {'answerText': '10', 'isCorrect': false},
+        {'answerText': '7', 'isCorrect': false},
+        {'answerText': '2', 'isCorrect': false},
+        {'answerText': '4', 'isCorrect': true},
+      ],
+    },
+    {
+      'question':
+          'How many states have J and B visited together (not including driving through)?',
+      'answers': [
+        {'answerText': '24', 'isCorrect': true},
+        {'answerText': '38', 'isCorrect': false},
+        {'answerText': '29', 'isCorrect': false},
+        {'answerText': '18', 'isCorrect': false},
+      ],
+    },
+    {
+      'question': 'What type of critter is the lucky bug from Mulan?',
+      'answers': [
+        {'answerText': 'Grasshopper', 'isCorrect': false},
+        {'answerText': 'Cricket', 'isCorrect': true},
+        {'answerText': 'Ladybug', 'isCorrect': false},
+        {'answerText': 'Firefly', 'isCorrect': false},
+      ]
+    },
+    {
+      'question':
+          'What tasty snack was not in the list of treats B used her dining dollars on to woo J?',
+      'answers': [
+        {'answerText': 'Chocolate-chip Muffin', 'isCorrect': false},
+        {'answerText': 'Howling Cow Wolf Tracks Milkshake', 'isCorrect': false},
+        {'answerText': "Post-work Reese's Fast Break", 'isCorrect': true},
+        {'answerText': 'Morning Chick-Fil-A', 'isCorrect': false},
+      ],
+    },
+    {
+      'question': 'How many years did J and B date long distance?',
+      'answers': [
+        {'answerText': '1', 'isCorrect': false},
+        {'answerText': '2', 'isCorrect': false},
+        {'answerText': '3', 'isCorrect': true},
+        {'answerText': '4', 'isCorrect': false},
+      ],
+    }
   ];
 }
