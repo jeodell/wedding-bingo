b2931fa33ee3f1f2a5452229fdc22a720db0d012
Jason O'Dell <jason@vitalflohealth.com>
2022-09-05 15:02:10 -0400

    Adding quiz question and rearranging

--- lib/data/quiz_data.dart
+++ lib/data/quiz_data.dart
@@ -1,18 +1,6 @@
 mixin QuizData {
   static final List<Map<String, dynamic>> quiz = <Map<String, dynamic>>[
     <String, dynamic>{
-      'question': 'What song did B tumble to in her 2001 recital?',
-      'answers': <Map<String, dynamic>>[
-        <String, dynamic>{'answerText': "A Bug's Life", 'isCorrect': true},
-        <String, dynamic>{'answerText': 'Hard Knock Life', 'isCorrect': false},
-        <String, dynamic>{'answerText': 'Baby Shark', 'isCorrect': false},
-        <String, dynamic>{
-          'answerText': 'Somewhere Over The Rainbow',
-          'isCorrect': false
-        },
-      ]
-    },
-    <String, dynamic>{
       'question': 'Where did sparks first fly?',
       'answers': <Map<String, dynamic>>[
         <String, dynamic>{'answerText': 'NC State', 'isCorrect': false},
@@ -23,6 +11,18 @@ mixin QuizData {
           'isCorrect': false
         },
       ],
+    },
+    <String, dynamic>{
+      'question': 'What song did B tumble to in her 2001 recital?',
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': "A Bug's Life", 'isCorrect': true},
+        <String, dynamic>{'answerText': 'Hard Knock Life', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Baby Shark', 'isCorrect': false},
+        <String, dynamic>{
+          'answerText': 'Somewhere Over The Rainbow',
+          'isCorrect': false
+        },
+      ]
     },
     <String, dynamic>{
       'question': "What is B's favorite feature about J?",
@@ -46,21 +46,21 @@ mixin QuizData {
       ],
     },
     <String, dynamic>{
+      'question': 'What was NOT on the first album J burned for B?',
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Sk8r Boi', 'isCorrect': true},
+        <String, dynamic>{'answerText': '', 'isCorrect': false},
+        <String, dynamic>{'answerText': '', 'isCorrect': false},
+        <String, dynamic>{'answerText': '', 'isCorrect': false},
+      ],
+    },
+    <String, dynamic>{
       'question': "Who are the enemies in the 1998 classic A Bug's Life?",
       'answers': <Map<String, dynamic>>[
         <String, dynamic>{'answerText': 'Wasps', 'isCorrect': false},
         <String, dynamic>{'answerText': 'Grasshoppers', 'isCorrect': true},
         <String, dynamic>{'answerText': 'Fleas', 'isCorrect': false},
         <String, dynamic>{'answerText': 'Caterpillars', 'isCorrect': false},
-      ],
-    },
-    <String, dynamic>{
-      'question': 'What was not on the first album J burned for B?',
-      'answers': <Map<String, dynamic>>[
-        <String, dynamic>{'answerText': 'Sk8r Boi', 'isCorrect': true},
-        <String, dynamic>{'answerText': '', 'isCorrect': false},
-        <String, dynamic>{'answerText': '', 'isCorrect': false},
-        <String, dynamic>{'answerText': '', 'isCorrect': false},
       ],
     },
     <String, dynamic>{
@@ -92,6 +92,16 @@ mixin QuizData {
           'answerText': 'We googled cute pet names for boyfriend/girlfriend',
           'isCorrect': false
         },
+      ],
+    },
+    <String, dynamic>{
+      'question':
+          'How many states have J and B visited together (not including driving through)?',
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': '24', 'isCorrect': true},
+        <String, dynamic>{'answerText': '38', 'isCorrect': false},
+        <String, dynamic>{'answerText': '29', 'isCorrect': false},
+        <String, dynamic>{'answerText': '18', 'isCorrect': false},
       ],
     },
     <String, dynamic>{
@@ -156,10 +166,10 @@ mixin QuizData {
     <String, dynamic>{
       'question': "What is J's favorite feature about B?",
       'answers': <Map<String, dynamic>>[
-        <String, dynamic>{'answerText': 'Short Stature', 'isCorrect': true},
         <String, dynamic>{'answerText': 'Poop-Brown Eyes', 'isCorrect': false},
         <String, dynamic>{'answerText': 'Large Nostrils', 'isCorrect': false},
         <String, dynamic>{'answerText': 'Short Eyelashes', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'All of the Above', 'isCorrect': true},
       ],
     },
     <String, dynamic>{
@@ -183,17 +193,7 @@ mixin QuizData {
       ],
     },
     <String, dynamic>{
-      'question':
-          'How many states have J and B visited together (not including driving through)?',
-      'answers': <Map<String, dynamic>>[
-        <String, dynamic>{'answerText': '24', 'isCorrect': true},
-        <String, dynamic>{'answerText': '38', 'isCorrect': false},
-        <String, dynamic>{'answerText': '29', 'isCorrect': false},
-        <String, dynamic>{'answerText': '18', 'isCorrect': false},
-      ],
-    },
-    <String, dynamic>{
-      'question': 'What type of critter is the lucky bug from Mulan?',
+      'question': 'What type of bug is the lucky bug from Mulan?',
       'answers': <Map<String, dynamic>>[
         <String, dynamic>{'answerText': 'Grasshopper', 'isCorrect': false},
         <String, dynamic>{'answerText': 'Cricket', 'isCorrect': true},
@@ -202,8 +202,26 @@ mixin QuizData {
       ]
     },
     <String, dynamic>{
+      'question': 'What was the first game J and B played together?',
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Mario Kart', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Connect 4', 'isCorrect': true},
+        <String, dynamic>{'answerText': 'Guess Who', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Destiny', 'isCorrect': false},
+      ],
+    },
+    <String, dynamic>{
+      'question': 'How many years did J and B date long distance?',
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': '1', 'isCorrect': false},
+        <String, dynamic>{'answerText': '2', 'isCorrect': false},
+        <String, dynamic>{'answerText': '3', 'isCorrect': true},
+        <String, dynamic>{'answerText': '4', 'isCorrect': false},
+      ],
+    },
+    <String, dynamic>{
       'question':
-          'What tasty snack was not in the list of treats B used her dining dollars on to woo J?',
+          'What tasty snack was NOT in the list of treats B used her dining dollars on to woo J?',
       'answers': <Map<String, dynamic>>[
         <String, dynamic>{
           'answerText': 'Chocolate-chip Muffin',
@@ -223,14 +241,5 @@ mixin QuizData {
         },
       ],
     },
-    <String, dynamic>{
-      'question': 'How many years did J and B date long distance?',
-      'answers': <Map<String, dynamic>>[
-        <String, dynamic>{'answerText': '1', 'isCorrect': false},
-        <String, dynamic>{'answerText': '2', 'isCorrect': false},
-        <String, dynamic>{'answerText': '3', 'isCorrect': true},
-        <String, dynamic>{'answerText': '4', 'isCorrect': false},
-      ],
-    }
   ];
 }
