73144344cd718781b1c4ec07980f0681890c9f4d
Jason O'Dell <jason@vitalflohealth.com>
2022-09-05 14:08:10 -0400

    Moooore linter warnings

--- lib/data/quiz_data.dart
+++ lib/data/quiz_data.dart
@@ -1,202 +1,235 @@
 mixin QuizData {
-  static final List<Map<String, dynamic>> quiz = [
-    {
+  static final List<Map<String, dynamic>> quiz = <Map<String, dynamic>>[
+    <String, dynamic>{
       'question': 'What song did B tumble to in her 2001 recital?',
-      'answers': [
-        {'answerText': "A Bug's Life", 'isCorrect': true},
-        {'answerText': 'Hard Knock Life', 'isCorrect': false},
-        {'answerText': 'Baby Shark', 'isCorrect': false},
-        {'answerText': 'Somewhere Over The Rainbow', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': "A Bug's Life", 'isCorrect': true},
+        <String, dynamic>{'answerText': 'Hard Knock Life', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Baby Shark', 'isCorrect': false},
+        <String, dynamic>{
+          'answerText': 'Somewhere Over The Rainbow',
+          'isCorrect': false
+        },
       ]
     },
-    {
+    <String, dynamic>{
       'question': 'Where did sparks first fly?',
-      'answers': [
-        {'answerText': 'NC State', 'isCorrect': false},
-        {'answerText': 'Calvary', 'isCorrect': false},
-        {'answerText': 'Lake Lure', 'isCorrect': true},
-        {'answerText': 'High School Graduation', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'NC State', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Calvary', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Lake Lure', 'isCorrect': true},
+        <String, dynamic>{
+          'answerText': 'High School Graduation',
+          'isCorrect': false
+        },
       ],
     },
-    {
+    <String, dynamic>{
       'question': "What is B's favorite feature about J?",
-      'answers': [
-        {'answerText': 'Long Back', 'isCorrect': false},
-        {'answerText': 'Bony Shoulders', 'isCorrect': false},
-        {'answerText': 'Big Brain', 'isCorrect': false},
-        {'answerText': 'All of the Above', 'isCorrect': true},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Long Back', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Bony Shoulders', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Big Brain', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'All of the Above', 'isCorrect': true},
       ],
     },
-    {
+    <String, dynamic>{
       'question': 'Where did J finally propose to B?',
-      'answers': [
-        {'answerText': 'Tahoe', 'isCorrect': false},
-        {'answerText': 'Wind River Range', 'isCorrect': false},
-        {'answerText': 'Maine', 'isCorrect': false},
-        {'answerText': 'Sawtooth Mountains', 'isCorrect': true},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Tahoe', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Wind River Range', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Maine', 'isCorrect': false},
+        <String, dynamic>{
+          'answerText': 'Sawtooth Mountains',
+          'isCorrect': true
+        },
       ],
     },
-    {
+    <String, dynamic>{
       'question': "Who are the enemies in the 1998 classic A Bug's Life?",
-      'answers': [
-        {'answerText': 'Wasps', 'isCorrect': false},
-        {'answerText': 'Grasshoppers', 'isCorrect': true},
-        {'answerText': 'Fleas', 'isCorrect': false},
-        {'answerText': 'Caterpillars', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Wasps', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Grasshoppers', 'isCorrect': true},
+        <String, dynamic>{'answerText': 'Fleas', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Caterpillars', 'isCorrect': false},
       ],
     },
-    {
+    <String, dynamic>{
       'question': 'What was not on the first album J burned for B?',
-      'answers': [
-        {'answerText': 'Sk8r Boi', 'isCorrect': true},
-        {'answerText': '', 'isCorrect': false},
-        {'answerText': '', 'isCorrect': false},
-        {'answerText': '', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Sk8r Boi', 'isCorrect': true},
+        <String, dynamic>{'answerText': '', 'isCorrect': false},
+        <String, dynamic>{'answerText': '', 'isCorrect': false},
+        <String, dynamic>{'answerText': '', 'isCorrect': false},
       ],
     },
-    {
+    <String, dynamic>{
       'question': 'Where was our first date?',
-      'answers': [
-        {'answerText': 'Morning Times', 'isCorrect': false},
-        {'answerText': 'Port City Java', 'isCorrect': false},
-        {'answerText': 'Cup a Joe', 'isCorrect': false},
-        {'answerText': 'Caribou Coffee', 'isCorrect': true},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Morning Times', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Port City Java', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Cup a Joe', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Caribou Coffee', 'isCorrect': true},
       ]
     },
-    {
+    <String, dynamic>{
       'question': 'Where did the term bugs come from?',
-      'answers': [
-        {
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{
           'answerText': 'When J received a random snapchat from HayBug1992',
           'isCorrect': true
         },
-        {
+        <String, dynamic>{
           'answerText': 'J put his hair up in two antennae-like sprouts',
           'isCorrect': false
         },
-        {
+        <String, dynamic>{
           'answerText':
               "B saw an inchworm on NC State's campus and said it looked like J",
           'isCorrect': false
         },
-        {
+        <String, dynamic>{
           'answerText': 'We googled cute pet names for boyfriend/girlfriend',
           'isCorrect': false
         },
       ],
     },
-    {
+    <String, dynamic>{
       'question': 'What is the most physical pain B has inflicted on J?',
-      'answers': [
-        {'answerText': 'Piercing his ear', 'isCorrect': true},
-        {
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Piercing his ear', 'isCorrect': true},
+        <String, dynamic>{
           'answerText':
               'Shoulder checking his chin while attempting to recreate the Dirty Dancing scene',
           'isCorrect': false
         },
-        {
+        <String, dynamic>{
           'answerText': 'Tackling him onto a bed of prong-exposed chargers',
           'isCorrect': false
         },
-        {
+        <String, dynamic>{
           'answerText': 'Hitting him in the balls with a nerf basketball',
           'isCorrect': false
         },
       ]
     },
-    {
+    <String, dynamic>{
       'question': 'Where was our first hike?',
-      'answers': [
-        {'answerText': 'Jordan Lake', 'isCorrect': false},
-        {'answerText': 'Umstead Park', 'isCorrect': false},
-        {'answerText': 'Hanging Rock', 'isCorrect': false},
-        {'answerText': 'Grandfather Mountain', 'isCorrect': true},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Jordan Lake', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Umstead Park', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Hanging Rock', 'isCorrect': false},
+        <String, dynamic>{
+          'answerText': 'Grandfather Mountain',
+          'isCorrect': true
+        },
       ],
     },
-    {
+    <String, dynamic>{
       'question': 'What was the first national park we visited?',
-      'answers': [
-        {'answerText': 'Hot Springs, AR', 'isCorrect': false},
-        {'answerText': 'Great Sand Dunes, CO', 'isCorrect': true},
-        {'answerText': 'Rocky Mountain, CO', 'isCorrect': false},
-        {'answerText': 'Great Smoky Mountains, NC', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Hot Springs, AR', 'isCorrect': false},
+        <String, dynamic>{
+          'answerText': 'Great Sand Dunes, CO',
+          'isCorrect': true
+        },
+        <String, dynamic>{
+          'answerText': 'Rocky Mountain, CO',
+          'isCorrect': false
+        },
+        <String, dynamic>{
+          'answerText': 'Great Smoky Mountains, NC',
+          'isCorrect': false
+        },
       ],
     },
-    {
+    <String, dynamic>{
       'question':
           'How many guys were seriously interested in B when J asked her out?',
-      'answers': [
-        {'answerText': '1', 'isCorrect': false},
-        {'answerText': '2', 'isCorrect': false},
-        {'answerText': '3', 'isCorrect': false},
-        {'answerText': '4', 'isCorrect': true},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': '1', 'isCorrect': false},
+        <String, dynamic>{'answerText': '2', 'isCorrect': false},
+        <String, dynamic>{'answerText': '3', 'isCorrect': false},
+        <String, dynamic>{'answerText': '4', 'isCorrect': true},
       ],
     },
-    {
+    <String, dynamic>{
       'question': "What is J's favorite feature about B?",
-      'answers': [
-        {'answerText': 'Short Stature', 'isCorrect': true},
-        {'answerText': 'Poop-Brown Eyes', 'isCorrect': false},
-        {'answerText': 'Large Nostrils', 'isCorrect': false},
-        {'answerText': 'Short Eyelashes', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Short Stature', 'isCorrect': true},
+        <String, dynamic>{'answerText': 'Poop-Brown Eyes', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Large Nostrils', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Short Eyelashes', 'isCorrect': false},
       ],
     },
-    {
+    <String, dynamic>{
       'question':
           'What famous dish did J make during his first Haferman Thanksgiving?',
-      'answers': [
-        {'answerText': 'Rolls', 'isCorrect': false},
-        {'answerText': 'Mac n Cheese', 'isCorrect': true},
-        {'answerText': 'Mashed Potatoes', 'isCorrect': false},
-        {'answerText': 'Green Beans', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Rolls', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Mac n Cheese', 'isCorrect': true},
+        <String, dynamic>{'answerText': 'Mashed Potatoes', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Green Beans', 'isCorrect': false},
       ],
     },
-    {
+    <String, dynamic>{
       'question':
           'How many times did J and B sleep in a car during their trips?',
-      'answers': [
-        {'answerText': '10', 'isCorrect': false},
-        {'answerText': '7', 'isCorrect': false},
-        {'answerText': '2', 'isCorrect': false},
-        {'answerText': '4', 'isCorrect': true},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': '10', 'isCorrect': false},
+        <String, dynamic>{'answerText': '7', 'isCorrect': false},
+        <String, dynamic>{'answerText': '2', 'isCorrect': false},
+        <String, dynamic>{'answerText': '4', 'isCorrect': true},
       ],
     },
-    {
+    <String, dynamic>{
       'question':
           'How many states have J and B visited together (not including driving through)?',
-      'answers': [
-        {'answerText': '24', 'isCorrect': true},
-        {'answerText': '38', 'isCorrect': false},
-        {'answerText': '29', 'isCorrect': false},
-        {'answerText': '18', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': '24', 'isCorrect': true},
+        <String, dynamic>{'answerText': '38', 'isCorrect': false},
+        <String, dynamic>{'answerText': '29', 'isCorrect': false},
+        <String, dynamic>{'answerText': '18', 'isCorrect': false},
       ],
     },
-    {
+    <String, dynamic>{
       'question': 'What type of critter is the lucky bug from Mulan?',
-      'answers': [
-        {'answerText': 'Grasshopper', 'isCorrect': false},
-        {'answerText': 'Cricket', 'isCorrect': true},
-        {'answerText': 'Ladybug', 'isCorrect': false},
-        {'answerText': 'Firefly', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': 'Grasshopper', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Cricket', 'isCorrect': true},
+        <String, dynamic>{'answerText': 'Ladybug', 'isCorrect': false},
+        <String, dynamic>{'answerText': 'Firefly', 'isCorrect': false},
       ]
     },
-    {
+    <String, dynamic>{
       'question':
           'What tasty snack was not in the list of treats B used her dining dollars on to woo J?',
-      'answers': [
-        {'answerText': 'Chocolate-chip Muffin', 'isCorrect': false},
-        {'answerText': 'Howling Cow Wolf Tracks Milkshake', 'isCorrect': false},
-        {'answerText': "Post-work Reese's Fast Break", 'isCorrect': true},
-        {'answerText': 'Morning Chick-Fil-A', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{
+          'answerText': 'Chocolate-chip Muffin',
+          'isCorrect': false
+        },
+        <String, dynamic>{
+          'answerText': 'Howling Cow Wolf Tracks Milkshake',
+          'isCorrect': false
+        },
+        <String, dynamic>{
+          'answerText': "Post-work Reese's Fast Break",
+          'isCorrect': true
+        },
+        <String, dynamic>{
+          'answerText': 'Morning Chick-Fil-A',
+          'isCorrect': false
+        },
       ],
     },
-    {
+    <String, dynamic>{
       'question': 'How many years did J and B date long distance?',
-      'answers': [
-        {'answerText': '1', 'isCorrect': false},
-        {'answerText': '2', 'isCorrect': false},
-        {'answerText': '3', 'isCorrect': true},
-        {'answerText': '4', 'isCorrect': false},
+      'answers': <Map<String, dynamic>>[
+        <String, dynamic>{'answerText': '1', 'isCorrect': false},
+        <String, dynamic>{'answerText': '2', 'isCorrect': false},
+        <String, dynamic>{'answerText': '3', 'isCorrect': true},
+        <String, dynamic>{'answerText': '4', 'isCorrect': false},
       ],
     }
   ];
